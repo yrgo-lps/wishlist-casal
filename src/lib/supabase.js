202aa64a7a0062cb4f6989342cb0bb36159df362
yrgo-lps <ygorlpsilva1@gmail.com>
2026-05-11 23:58:59 -0300

    Create supabase.js

---
+++ src/lib/supabase.js
@@ -0,0 +1,10 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error('Variáveis de ambiente do Supabase não configuradas.')
+}
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
